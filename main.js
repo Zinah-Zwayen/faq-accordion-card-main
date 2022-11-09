290b62f0931fe1d2db6ea9e79208a181059a32f1
Zinah-AZ <105180491+Zinah-Zwayen@users.noreply.github.com>
2022-11-09 12:56:24 +1000

    Add files via upload

--- main.js
+++ main.js
@@ -1,11 +1,10 @@
 const acc = document.getElementsByClassName("Question");
-const arrow = document.querySelector(".arrow");
 
 for (let i = 0; i < acc.length; i++) {
     acc[i].addEventListener("click", function() {
-    /* Toggle between adding and removing the "active" class,
-    to highlight the button that controls the panel */
-    this.classList.toggle("active");
+    /* Toggle between adding and removing the "open" class,
+    to highlight the button that controls the Answer */
+    this.classList.toggle("open");
     
     /* Toggle between hiding and showing the active panel */
     const Answer = this.nextElementSibling;
@@ -15,7 +14,6 @@ for (let i = 0; i < acc.length; i++) {
         Answer.style.display = "block";
 
     }
-    arrow.classList.toggle("rotate");
     });
     
 }
