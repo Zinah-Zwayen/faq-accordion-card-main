5d083ab76b5d9accf0879d1afc3afd43525b6dd0
Zinah-AZ <105180491+Zinah-Zwayen@users.noreply.github.com>
2022-11-10 11:31:19 +1000

    Add files via upload

--- main.js
+++ main.js
@@ -1,23 +1,29 @@
 const acc = document.getElementsByClassName("Question");
+const answers = document.getElementsByClassName("Answer");
 
 for (let i = 0; i < acc.length; i++) {
     acc[i].addEventListener("click", function() {
+
     /* Toggle between adding and removing the "open" class,
     to highlight the button that controls the Answer */
     this.classList.toggle("open");
     
-    /* Toggle between hiding and showing the active panel */
+
+  /* Toggle between hiding and showing the active panel */
     const Answer = this.nextElementSibling;
     if (Answer.style.display === "block") {
         Answer.style.display = "none";
     } else {
+        /* close all panel */
+        for (let i = 0; i < answers.length; i++) {
+            answers[i].style.display = 'none';
+        }
         Answer.style.display = "block";
+        acc.style.borderBottom = '1px solid white';
 
     }
-    });
     
+    });
 }
-
-
 
 
